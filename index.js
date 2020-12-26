a1134af302b70dc890ebfa087f177db3b8a7f6ac
Andrei Cerbulescu <andrei.cerbulescu@gmail.com>
2020-12-26 14:40:39 +0200

    no message

---
+++ index.js
@@ -0,0 +1,70 @@
+// app.js
+const express = require('express')
+const bodyParser = require('body-parser');
+const fs = require('fs')
+const ejs = require('ejs')
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+var mysql = require('mysql');
+
+const saltRounds = 10;
+
+// Create Express app
+const app = express()
+
+var dbCon = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "123456",
+    database: "OnlineGame"
+});
+
+dbCon.connect(function (err) {
+    if (err) {
+        console.log(err);
+    }
+
+    else {
+        console.log("Connected to the database!")
+    }
+
+})
+
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+app.use(session({ secret: 'ssshhhhh' })); // mandatory
+
+app.get('/', (req, res) => {
+
+    dbCon.query("SHOW TABLES", function (err, result, fields) {
+
+        console.log(result)
+
+        res.render('index.ejs',{
+            tabele: result
+        })
+
+
+    })
+
+
+})
+
+app.get('/selectareTabel', (req, res)=>{
+    var tabela = req.query.tabela;
+    
+    dbCon.query("SELECT * FROM "+tabela, function(err, result, fields){
+
+        console.log(result, fields)
+        res.render('afisareTabele.ejs', {
+            continut: result,
+            coloane: fields
+        })
+
+    })
+
+})
+
+app.listen(3000, () => console.log('Server running on port 3000!'))
